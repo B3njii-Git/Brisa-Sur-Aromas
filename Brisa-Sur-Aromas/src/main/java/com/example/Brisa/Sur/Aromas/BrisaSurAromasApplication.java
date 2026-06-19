ad6f2e625eac14390e8284595b3a967c5e41f166
Benjamin Guerrero <benguerrerov@duocuc.cl>
2026-06-19 09:05:28 -0400

    subida de proyecto

---
+++ Brisa-Sur-Aromas/src/main/java/com/example/Brisa/Sur/Aromas/BrisaSurAromasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Brisa.Sur.Aromas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrisaSurAromasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrisaSurAromasApplication.class, args);
+	}
+
+}
---
+++ Brisa-Sur-Aromas/src/test/java/com/example/Brisa/Sur/Aromas/BrisaSurAromasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Brisa.Sur.Aromas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrisaSurAromasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
